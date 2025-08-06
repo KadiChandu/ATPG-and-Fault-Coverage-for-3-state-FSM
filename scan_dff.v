9ab0f3b0e8b369350278d8031ec5fc78cdc217ff
Kadi Chandu <chandukadi111@gmail.com>
2025-08-06 10:54:28 +0530

    Update scan_dff.v

--- scan_dff.v
+++ scan_dff.v
@@ -1,19 +1,31 @@
-module scan_dff (
-    input clk, rst, scan_in, scan_enable,
-    input d,
-    output q );
-    wire dmux_out, not_rst, d_rst;
-    not NR(not_rst, rst);
-    and A1(d_rst, dmux_out, not_rst);
-    // Mux for scan
-    wire nSE, d1, d2;
-    not N1(nSE, scan_enable);
-    and A2(d1, D, nSE);
-    and A3(d2, scan_in, scan_enable);
-    or  O1(Dmux_out, d1, d2);
-    // D Flip-Flop with Reset
-    wire Q_internal, nclk;
-    not N2(nclk, clk);
-    d_latch DL1 (.D(D_rst), .clk(nclk), .Q(Q_internal));
-    d_latch DL2 (.D(Q_internal), .clk(clk), .Q(Q));
+module scan_dff(q, d, scan_in, scan_enable, clk, rst);
+  input d, scan_in, scan_enable, clk, rst;
+  output q;
+  wire mux_out;
+  // mux: mux_out = scan_enable ? scan_in : d 
+  wire nse, and1_out, and2_out;
+  not  u1(nse, scan_enable);
+  and  u2(and1_out, d, nse);
+  and  u3(and2_out, scan_in, scan_enable);
+  or   u4(mux_out, and1_out, and2_out);
+  wire clk_n;
+  not u5(clk_n, clk);
+  // master latch
+  wire m1, m2, m3, m4;
+  nand u6(m1, mux_out, clk_n);
+  nand u7(m2, m1, clk_n);
+  nand u8(m3, m1, m4);
+    nand u9(m4, m2, m3);  // latch output
+  // slave latch
+  wire s1, s2, s3, s4;
+  nand u10(s1, m2, clk);
+  nand u11(s2, s1, clk);
+  nand u12(s3, s1, s4);
+    nand u13(s4, s2, s3);  // output before reset
+  // async reset logic (active high)
+  wire nrst, q_reset_n, q_n;
+  not  u14(nrst, rst);
+  nand u15(q_reset_n, s4, nrst);
+  nand u16(q_n, q_reset_n, q_reset_n); // buffer
+  nand u17(q, q_n, q_n);               // final output
 endmodule
